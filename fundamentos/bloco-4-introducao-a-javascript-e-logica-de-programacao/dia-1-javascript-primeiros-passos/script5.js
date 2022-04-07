ee3973c0597d7ba949f080773782c6c3a1bb41e2
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-07 09:47:58 +0100

    correcting question 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script5.js
@@ -2,6 +2,10 @@ const angleOne = 45;
 const angleTwo = 30;
 const angleThree = 105;
 
+if (angleOne || angleTwo || angleThree < 0) {
+    console.log('there is an invalid angle.');
+}
+
 if (angleOne + angleTwo + angleThree == 180) {
     console.log(true);
 } else {

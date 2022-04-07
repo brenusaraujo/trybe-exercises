a4b8d4b7f23facd428fd42731835ae864f436600
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-07 19:17:44 +0100

    resolve questao 1, 2 e 3

---
+++ fundamentos/bloco-4-introducao-a-javascript/script.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+} //questao 1
+
+for (i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+}
+console.log(sum); //questao 2
+
+console.log(sum / numbers.length); //questao 3

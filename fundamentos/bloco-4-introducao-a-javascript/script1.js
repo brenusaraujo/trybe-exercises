6f1754219308e523967b5528c827b8f08f3b110f
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-08 20:11:57 +0100

    resolve questao 4

--- fundamentos/bloco-4-introducao-a-javascript/script1.js
+++ fundamentos/bloco-4-introducao-a-javascript/script1.js
@@ -37,3 +37,24 @@ for (let index = 0; index < array.length; index += 1) {
     }
 }
 console.log(storage)
+
+//fim da questão 3
+
+//questão 4
+
+let biggestPrimeNumber = 0;
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+    let isPrime = true;
+    for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+        if (currentNumber % currentDivisor === 0) {
+            isPrime = false;
+        }
+    }
+    if (isPrime) {
+        biggestPrimeNumber = currentNumber;
+    }
+}
+
+console.log(biggestPrimeNumber);
+
+//fim da questão 4

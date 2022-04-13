06a1db01b0dfadef8c909f5a4f81eb26017a1b90
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-14 00:53:07 +0100

    teste

---
+++ fundamentos/bloco-5-javascript-dom-eventos-webstorage/script.js
@@ -0,0 +1,4 @@
+document.getElementById("header-container").style.color = "white";
+document.getElementById("header-container").style.backgroundColor = "rgb(60, 179, 113)"
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "rgb(255, 213, 128)";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "rgb(195, 33, 201)";

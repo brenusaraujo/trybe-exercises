088851290ee9f458c217335280013ca33a3441d5
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-14 11:29:07 +0100

    resolve todas as questoes de elementos

--- fundamentos/bloco-5-javascript-dom-eventos-webstorage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-webstorage/script.js
@@ -2,3 +2,6 @@ document.getElementById('elementoOndeVoceEsta').parentNode.style.color = 'blue'
 document.getElementById('primeiroFilhoDoFilho').innerText = 'Teste';
 document.getElementById('pai').firstChild;
 document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+document.getElementById('elementoOndeVoceEsta').nextSibling;
+document.getElementById('elementoOndeVoceEsta').nextElementSibling;
+document.getElementById('pai').children[2];

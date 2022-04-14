57b647adb929c040778fcd38693dfdebf23ba4fd
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-14 11:19:04 +0100

    resolve questao 5

--- fundamentos/bloco-5-javascript-dom-eventos-webstorage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-webstorage/script.js
@@ -1,2 +1,4 @@
 document.getElementById('elementoOndeVoceEsta').parentNode.style.color = 'blue'
 document.getElementById('primeiroFilhoDoFilho').innerText = 'Teste';
+document.getElementById('pai').firstChild;
+document.getElementById('elementoOndeVoceEsta').previousElementSibling;

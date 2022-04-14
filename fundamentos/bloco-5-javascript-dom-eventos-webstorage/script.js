1f2be6680cb74f7d92e7e2f90c2a791f4e069d34
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-14 19:13:40 +0100

    cria filho para elementoOndeVoceEsta

--- fundamentos/bloco-5-javascript-dom-eventos-webstorage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-webstorage/script.js
@@ -5,3 +5,11 @@ document.getElementById('elementoOndeVoceEsta').previousElementSibling;
 document.getElementById('elementoOndeVoceEsta').nextSibling;
 document.getElementById('elementoOndeVoceEsta').nextElementSibling;
 document.getElementById('pai').children[2];
+
+let sibling = document.querySelector('#elementoOndeVoceEsta');
+
+let siblingTest = document.createElement("p");
+
+siblingTest.innerHTML = "Breno araújo de matos";
+
+sibling.appendChild(siblingTest);

990f67701d26d95c0a96cb4243fcba10c848bd4c
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-30 17:23:00 +0100

    faz os extras com o script

--- fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/script.js
@@ -1,5 +1,15 @@
 const button = document.getElementById('button');
-
+const myform = document.getElementById('my_form')
 button.addEventListener('click', function(event) {
     event.preventDefault();
+    myform.reset(); //reseta o form
 })
+
+
+const pictures = document.getElementById('pictures');
+
+if (pictures) {
+
+} else {
+
+}

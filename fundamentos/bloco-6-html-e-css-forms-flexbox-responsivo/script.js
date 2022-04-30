3d61ed6c2472e611e06b36fc02b86fc739da6086
brenusaraujo <brenoaraujo50@icloud.com>
2022-04-30 18:16:17 +0100

    resolve bonus 1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/script.js
@@ -1,15 +1,17 @@
 const button = document.getElementById('button');
-const myform = document.getElementById('my_form')
+const myform = document.getElementById('my_form');
+const agree = document.getElementById('agree');
 button.addEventListener('click', function(event) {
     event.preventDefault();
     myform.reset(); //reseta o form
 })
 
-
-const pictures = document.getElementById('pictures');
-
-if (pictures) {
-
-} else {
-
+function enableSubmit() {
+    const button = document.querySelector('#button');
+    const agree = document.querySelector('#agree');
+    button.disabled = !agree.checked; //aqui, se o agree tiver marcado, o botao será desabilitado. se nao tiver marcado, sera habilitado.
 }
+window.onload = function() {
+    const agreement = document.getElementById('agree');
+    agreement.addEventListener('change', enableSubmit);
+};

e7ae7a76c81fe4eb6c2b1c59a043c61d8ed11171
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-05-10 23:14:21 +0100

    resolve parte 3

--- fundamentos/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,6 @@
 const myRemove = require('./myremove');
 const sum = require('./sum');
+const myFizzBuzz = require('./myfizzbuzz');
 
 test('sums two values', () => {
   expect(sum(4, 5)).toEqual(9);
@@ -11,4 +12,12 @@ test('retorna o array esperado', () => {
   expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]); // equal para array.
   expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]); // equal para array.
   expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]); // equal para array.
+});
+
+test('test fizzBuzz', () => {
+  expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  expect(myFizzBuzz(27)).toBe('fizz');
+  expect(myFizzBuzz(25)).toBe('buzz');
+  expect(myFizzBuzz(28)).toBe(28);
+  expect(myFizzBuzz(true)).toBe(false);
 });

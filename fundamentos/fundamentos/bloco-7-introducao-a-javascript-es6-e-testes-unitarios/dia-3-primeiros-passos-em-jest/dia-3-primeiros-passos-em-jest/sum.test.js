9a5d734f8404d3b2d98246cacf842d3ef0c92cda
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-05-10 22:32:06 +0100

    adiciona test para throw

--- fundamentos/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -3,5 +3,5 @@ const sum = require('./sum');
 test('sums two values', () => {
   expect(sum(4, 5)).toEqual(9);
   expect(sum(0, 0)).toEqual(0);
-
+  expect(sum).toThrowError(new Error('parameters must be numbers'));
 });

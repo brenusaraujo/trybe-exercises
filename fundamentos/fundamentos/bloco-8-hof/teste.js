92f2e6cdc63e21664529a0013cdf2bc08dc07f0d
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-05-19 17:05:40 +0100

    resolve sexta e sétima questões dos exercicios do dia

--- fundamentos/fundamentos/bloco-8-hof/teste.js
+++ fundamentos/fundamentos/bloco-8-hof/teste.js
@@ -56,3 +56,40 @@ const myList = [1, 2, 3];
 const swap = ([a, b, c]) => [c, b, a];
 
 // escreva swap abaixo
+
+// 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+// Dica: use array destructuring e abbreviation object literal.
+
+const toObject = ([marca, modelo, ano]) => ({ marca, modelo, ano });
+
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+console.log(toObject(shelbyCobra));
+
+// escreva toObject abaixo
+const ships = [{
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+
+const shipLength = ({ name, length, measurementUnit }) => `The ${name} is ${length} ${measurementUnit} long.`;
+// escreva shipLength abaixo
+
+console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'

cbf82ae554f1c10b213659e61b8a5ed4d7b78d04
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-05-19 11:31:17 +0100

    resolve quarta questao dos exercicios do dia

--- fundamentos/fundamentos/bloco-8-hof/teste.js
+++ fundamentos/fundamentos/bloco-8-hof/teste.js
@@ -18,3 +18,36 @@ const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and l
 
 console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
 console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+const people = [{
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = () => people.filter(item => item.nationality === 'Australian')
+  .filter(number => number.bornIn > 1901 && number.bornIn <= 2000);
+
+
+console.log(filterPeople());

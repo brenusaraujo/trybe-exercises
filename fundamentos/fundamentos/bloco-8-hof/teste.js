cb983b69d16bd7ac2b960e547a24d38b89c2ce7a
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-05-19 17:18:32 +0100

    resolve nona questao dos exercicios do course

--- fundamentos/fundamentos/bloco-8-hof/teste.js
+++ fundamentos/fundamentos/bloco-8-hof/teste.js
@@ -100,3 +100,16 @@ const greet = (name, greeting = 'Hi') => `${greeting}, ${name}!`;
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
 console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const { spring, summer, autumn, winter } = yearSeasons
+
+const yearMonths = [...winter, ...spring, ...summer, ...autumn];
+console.log(yearMonths);

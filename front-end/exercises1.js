ff5197d62db91d18deacc174fec6fa3a861583e3
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-05-25 10:26:34 +0100

    resolve exercicios do dia

--- front-end/exercises1.js
+++ front-end/exercises1.js
@@ -48,3 +48,50 @@ sendMarsTemperature(temperatureInFahrenheit, handleError);
 
 // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
 // sendMarsTemperature(greet, handleError);
+
+const pokemons = [{
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+getPokemonDetails(
+  (pokemon) => pokemon.name === 'Bulbasaur',
+  (error, message) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(message);
+    }
+  }
+);
+
+module.exports = {
+  getPokemonDetails,
+};
--- front-end/script.test.js
+++ front-end/script.test.js
@@ -1,13 +1,25 @@
-const asyncSum = require('./script');
+const { getPokemonDetails } = require("./exercises1");
 
+describe("A função getPokemonDetails", () => {
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
+    const expectedError = new Error('Não temos esse pokémon para você :(');
 
-test('Testando asyncSum, soma 5 mais 10', (done) => {
-  asyncSum(5, 10, (result) => {
-    try {
-      expect(result).toBe(15);
-      done();
-    } catch (error) {
-      done(error);
+    function callback(error, result) {
+      expect(error).toEqual(expectedError);
+      done(result);
     }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Pikachu', callback);
+  });
+
+  it("retorna um pokemon que existe no banco de dados", (done) => {
+    const expectedString = 'Olá, seu pokémon é o Charmander, o tipo dele é Fire e a habilidade principal dele é Ember';
+
+    function callback(err, result) {
+      expect(result).toBe(expectedString);
+      done(err);
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Charmander', callback);
   });
 });

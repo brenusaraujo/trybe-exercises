be13c5889e3661797c9a04926df6889c142b1cb5
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-06-01 11:01:22 +0100

    resolve questoes do dia

--- front-end/bloco-09-javascript-e-testes-assincronos/javascript-assincrono/api.js
+++ front-end/bloco-09-javascript-e-testes-assincronos/javascript-assincrono/api.js
@@ -1,27 +1,60 @@
 const API_URL = `https://api.coincap.io/v2/assets`
 
+const fetchUSDtoBRL = async() => {
+  const URL = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data.usd.brl;
+}
+
 const fetchCoin = async() => {
   const myObject = {
     method: 'GET',
     headers: { 'Accept': 'application/json' }
-  };
-  await fetch(API_URL, myObject)
-    .then(response => response.json())
-    .then(data => {
-      const tenthCoins = data.data.filter(coin => Number(coin.rank) <= 10).sort((a, b) => b.rank - a.rank);
-      const loop = () => {
-        for (let index = tenthCoins.length - 1; index >= 0; index -= 1) {
-          let teste = document.createElement('li');
-          teste.innerText = `${tenthCoins[index].name} (${tenthCoins[index].symbol}): ${tenthCoins[index].priceUsd}`;
-          document.getElementById('coinsCointainer').appendChild(teste);
-        }
-      };
+  }
+  const usdValue = await fetchUSDtoBRL();
+  try {
+    const response = await fetch(API_URL, myObject);
+    const data = await response.json();
+    const tenCurrencies = await data.data.filter(coin => Number(coin.rank) <= 10).sort((a, b) => b.rank - a.rank);
+    for (let index = tenCurrencies.length - 1; index >= 0; index -= 1) {
+      let teste = document.createElement('li');
+      teste.innerText = `${tenCurrencies[index].name} (${tenCurrencies[index].symbol}): R$ ${tenCurrencies[index].priceUsd*usdValue}`;
+      document.getElementById('coinsCointainer').appendChild(teste);
+    }
+  } catch (error) {
+    console.log(`something went wrong. ${error}`);
+  }
 
-      loop();
+}
 
 
-    });
+// const fetchCoin = async() => {
+//   const myObject = {
+//     method: 'GET',
+//     headers: { 'Accept': 'application/json' }
+//   };
+//   await fetch(API_URL, myObject)
+//     .then(response => response.json())
+//     .then(data => {
+//       const tenthCoins = data.data.filter(coin => Number(coin.rank) <= 10).sort((a, b) => b.rank - a.rank);
+//       const loop = () => {
+//         for (let index = tenthCoins.length - 1; index >= 0; index -= 1) {
+//           let teste = document.createElement('li');
+//           teste.innerText = `${tenthCoins[index].name} (${tenthCoins[index].symbol}): ${tenthCoins[index].priceUsd}`;
+//           document.getElementById('coinsCointainer').appendChild(teste);
+//         }
+//       };
 
-};
+//       loop();
 
-window.onload = () => fetchCoin();
+
+//     });
+
+// };
+
+
+
+
+
+window.onload = () => { fetchCoin(), fetchUSDtoBRL() };
--- front-end/bloco-09-javascript-e-testes-assincronos/javascript-assincrono/fetch.js
+++ front-end/bloco-09-javascript-e-testes-assincronos/javascript-assincrono/fetch.js
@@ -1,11 +1,22 @@
 const fetch = require('node-fetch');
 
-const fetchJoke = () => {
-  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data.value));
-}
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value));
+// }
 
-fetchJoke();
+// fetchJoke();
+
+const fetchProducts = async(query) => {
+  const urltest = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+
+  const response = await fetch(urltest);
+  const results = await response.json();
+  return results;
+
+};
+
+fetchProducts(computer);

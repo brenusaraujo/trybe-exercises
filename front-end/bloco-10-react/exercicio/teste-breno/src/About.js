27656e9a4c08e2f70408d1cd7136b209e4b3fdc2
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-06-21 15:09:21 +0100

    resolve primeiro requisiotop da questao 3

---
+++ front-end/bloco-10-react/exercicio/teste-breno/src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About () {
+  return (
+  <div>
+    <h1> Breno Araújo</h1>
+    <p> me chamo breno, moro em lisboa e sou casado. </p>
+  </div>
+  )
+}
+
+
+export default About;

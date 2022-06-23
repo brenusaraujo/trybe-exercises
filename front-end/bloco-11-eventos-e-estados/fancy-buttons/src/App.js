c65bddcae34d7abbb9fdb48d3aff7e98578afa9f
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-06-23 16:21:53 +0100

    resolve terceira parte do course

--- front-end/bloco-11-eventos-e-estados/fancy-buttons/src/App.js
+++ front-end/bloco-11-eventos-e-estados/fancy-buttons/src/App.js
@@ -12,22 +12,32 @@ import './App.css';
 class App extends React.Component {
   constructor(){
     super()
-    this.state ={
-      numeroDeCliques:0
+    this.state = {
+      numeroDeCliques: 0,
+      corAtual: 'white',
     }
     this.handleClick1 = this.handleClick1.bind(this);
   }
    handleClick1() {
     this.setState((estadoAnterior, _props) => ({
-      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
     }));
+    if(this.state.numeroDeCliques % 2===0) {
+      this.setState((estadoAnterior, _props)=>({
+        corAtual: 'green',
+      }));
+    } else{
+      this.setState((estadoAnterior, _props)=>({
+        corAtual: 'red',
+      }));
+    }
+    console.log(this.state.corAtual);
   }
 
   render() {
     return (
       <div>
-        <button onClick = { this.handleClick1 } >{this.state.numeroDeCliques} </button>
-        
+        <button onClick = { this.handleClick1 } style={{ backgroundColor: `${this.state.corAtual}`, color: 'white'} }>{this.state.numeroDeCliques} </button>
       </div>
     )
   }

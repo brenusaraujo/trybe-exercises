c656b674a57b0bf0df60c7bded5536792ed60848
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-06-23 15:56:04 +0100

    resolve segunda parte do course

--- front-end/bloco-11-eventos-e-estados/fancy-buttons/src/App.js
+++ front-end/bloco-11-eventos-e-estados/fancy-buttons/src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
 import './App.css';
 
-function handleClick1() {
-  console.log( 'Cliclou no botão 1' );
-}
-function handleClick2() {
-  console.log( 'Cliclou no botão 2' );
-}
-function handleClick3() {
-  console.log( 'Cliclou no botão 3' );
-}
+
+// function handleClick2() {
+//   console.log( 'Cliclou no botão 2' );
+// }
+// function handleClick3() {
+//   console.log( 'Cliclou no botão 3' );
+// }
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      numeroDeCliques:0
+    }
+    this.handleClick1 = this.handleClick1.bind(this);
+  }
+   handleClick1() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }));
+  }
+
   render() {
     return (
       <div>
-        <button onClick = { handleClick1 } > Botão 1 </button>
-        <button onClick = { handleClick2 } > Botão 2 </button>
-        <button onClick = { handleClick3 } > Botão 3 </button>
+        <button onClick = { this.handleClick1 } >{this.state.numeroDeCliques} </button>
+        
       </div>
     )
   }

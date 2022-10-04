2dd8383ee2bf9d1f3bc76cca674646da54ae9dc8
Breno Araujo <brenoaraujo50@icloud.com>
2022-10-03 21:11:29 -0300

    adiciona rotas de transaction

--- backend/bloco-22-node-js/exercise22.5/src/db/peopleDb.js
+++ backend/bloco-22-node-js/exercise22.5/src/db/peopleDb.js
@@ -9,4 +9,19 @@ const list = () => connection.execute(
   'SELECT * FROM people',
 );
 
-module.exports = { insert, list };
+const listById = (id) => connection.execute(
+  `SELECT first_name FROM people 
+    WHERE id = ?`, [id],
+);
+
+const listTransactions = () => connection.execute(
+  'SELECT * FROM transactions',
+);
+
+const insertTransaction = (transaction) => connection.execute(
+  `INSERT INTO transactions (name, description, price, type)
+    VALUES (?, ?, ?, ?, ?)`, [transaction.name, 
+      transaction.description, transaction.price, transaction.price],
+);
+
+module.exports = { insert, list, listById, listTransactions, insertTransaction };
--- backend/bloco-22-node-js/exercise22.5/src/routes/peopleRoutes.js
+++ backend/bloco-22-node-js/exercise22.5/src/routes/peopleRoutes.js
@@ -6,9 +6,7 @@ const peopleRouter = express.Router();
 peopleRouter.post('/persons', async (req, res) => {
   try {
     const person = req.body;
-
     const [{ insertId }] = await peopleDb.insert(person);
-
     return res.status(201).json({ insertedId: insertId });
   } catch (error) {
     console.log(error.message);
@@ -16,14 +14,53 @@ peopleRouter.post('/persons', async (req, res) => {
   }
 });
 
+peopleRouter.post('/transactions', async (req, res) => {
+  try {
+    const transaction = req.body;
+
+    const returnedTransaction = await peopleDb.insertTransaction(transaction);
+    console.log(returnedTransaction);
+    return res.status(201).json({ message: `transaction ${transaction}succesful made` });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: `${error.message}` });
+  }
+});
+
 peopleRouter.get('/persons', async (_req, res) => {
   try {
-    const [persons] = await peopleDb.consult();
-
-    return res.status(200).json(persons);
+    const [persons] = await peopleDb.list();
+    if (persons) {
+      return res.status(200).json(persons);
+    } 
+      return res.status(400).json({ message: 'person not found' });
   } catch (error) {
     console.log(error.message);
     return res.status(500).json(error.message);
+  }
+});
+
+peopleRouter.get('/persons/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [person] = await peopleDb.listById(id);
+    if (person.length === 0) {
+      return res.status(400).json({ message: 'person not found' });
+    } 
+    return res.status(200).json(person[0].first_name);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(error.message);
+  }
+});
+
+peopleRouter.get('/transactions', async (_req, res) => {
+  try {
+    const transactions = await peopleDb.listTransactions();
+    return res.status(200).json(transactions);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: `error: ${error.message}` });
   }
 });
 

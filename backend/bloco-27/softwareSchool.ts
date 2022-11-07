99dd7f6038c4a3b8b449b3fbf6f4d8e04ff2d6a6
Breno Araujo <brenoaraujo50@icloud.com>
2022-11-07 19:41:32 -0300

    adiciona o contructor e os atributos

---
+++ backend/bloco-27/softwareSchool.ts
@@ -0,0 +1,24 @@
+class studentEnrolled {
+  enrollment: number;
+  name: string;
+  examGrade1: number;
+  examGrade2: number;
+  examGrade3: number;
+  examGrade4: number;
+  constructor(
+    e: number,
+    n: string,
+    eg1: number,
+    eg2: number,
+    eg3: number,
+    eg4: number
+  ) {
+    this.enrollment = e;
+    this.name = n;
+    this.examGrade1 = eg1;
+    this.examGrade2 = eg2;
+    this.examGrade3 = eg3;
+    this.examGrade4 = eg4;
+
+  }
+}

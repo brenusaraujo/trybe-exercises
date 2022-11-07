3bad947f44e3cad269985b1f0a5f157bc1ae7b77
Breno Araujo <brenoaraujo50@icloud.com>
2022-11-07 20:03:20 -0300

    adiciona os gettes

--- backend/bloco-27/softwareSchool.ts
+++ backend/bloco-27/softwareSchool.ts
@@ -19,6 +19,37 @@ class studentEnrolled {
     this.examGrade2 = eg2;
     this.examGrade3 = eg3;
     this.examGrade4 = eg4;
+  }
 
+  public get firstGrade(): number {
+    return this.examGrade1;
+  }
+  public get secondGrade(): number {
+    return this.examGrade2;
+  }
+  public get thirdGrade(): number {
+    return this.examGrade3;
+  }
+  public get fourthGrade(): number {
+    return this.examGrade4;
+  }
+
+  public get getEnrollment(): number {
+    return this.enrollment;
+  }
+
+  public get getName(): string {
+    return this.name;
+  }
+
+  public get sumAllGrades(): number {
+    return (
+      this.examGrade1 + this.examGrade2 + this.examGrade3 + this.examGrade4
+    );
+  }
+
+  public get average(): number {
+    const sumAllGrades = this.sumAllGrades;
+    return sumAllGrades / 4;
   }
 }

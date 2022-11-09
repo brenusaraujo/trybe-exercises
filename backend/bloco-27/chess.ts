28523ab121793ff851d70c05ee8dbfb04ed406e0
Breno Araujo <brenoaraujo50@icloud.com>
2022-11-09 10:30:59 -0300

    adiciona interfaces

---
+++ backend/bloco-27/chess.ts
@@ -0,0 +1,33 @@
+interface Logger {
+  log(string: string): void;
+}
+
+class ConsoleLogger implements Logger {
+  // constructor(parameters) {}
+  log(string: string): void {
+    console.log(string);
+  }
+}
+
+class ConsoleLogger2 implements Logger {
+  log(string: string): void {
+    console.log("logger2", string);
+  }
+  // constructor(parameters) {
+
+  // }
+}
+
+interface Database {
+  logger: Logger;
+  save(key: string, value: string): void;
+}
+
+class ExampleDatabase implements Database {
+  constructor(public logger: ConsoleLogger) {}
+
+  save(key: string, value: string): void {
+    this.logger.log(key);
+    this.logger.log(value);
+  }
+}

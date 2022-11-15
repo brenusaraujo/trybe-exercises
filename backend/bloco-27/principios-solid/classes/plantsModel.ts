f558b4f6d8eabffc65b189f6514128cd2266e9db
Breno Araujo <brenoaraujo50@icloud.com>
2022-11-15 14:31:51 -0300

    adiciona gitignore

--- backend/bloco-27/principios-solid/classes/plantsModel.ts
+++ backend/bloco-27/principios-solid/classes/plantsModel.ts
@@ -3,6 +3,21 @@ import Database from "../interfaces/database";
 
 export default class PlantsModel implements Database {
   constructor(parameters) {}
+  getPlants(): Promise<IPlant[]> {
+    throw new Error("Method not implemented.");
+  }
+  getPlantById(id: string): Promise<IPlant | null> {
+    throw new Error("Method not implemented.");
+  }
+  removePlantById(id: string): Promise<IPlant | null> {
+    throw new Error("Method not implemented.");
+  }
+  getPlantsThatNeedsSunWithId(id: string): Promise<IPlant[]> {
+    throw new Error("Method not implemented.");
+  }
+  editPlant(plantId: string, newPlant: IPlant): Promise<IPlant> {
+    throw new Error("Method not implemented.");
+  }
   async savePlant(plant: IPlant): Promise<IPlant> {
     throw new Error("Method not implemented.");
   }

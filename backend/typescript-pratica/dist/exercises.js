9bb567159084b2c70621223bc13abad7a8cbb846
Breno Araujo <brenoaraujo50@icloud.com>
2022-12-13 21:20:09 +0000

    faz os exemplos

---
+++ backend/typescript-pratica/dist/exercises.js
@@ -0,0 +1,27 @@
+"use strict";
+// ./exercises.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.triangleCheck = exports.getPolygonPerimeter = exports.getTriangleArea = exports.getRectangleArea = exports.getSquareArea = void 0;
+function getSquareArea(side) {
+    return side ** 2;
+}
+exports.getSquareArea = getSquareArea;
+function getRectangleArea(side, base) {
+    return side * base;
+}
+exports.getRectangleArea = getRectangleArea;
+function getTriangleArea(height, base) {
+    return (height * base) / 2;
+}
+exports.getTriangleArea = getTriangleArea;
+function getPolygonPerimeter(sides) {
+    return sides.reduce((curr, acc) => curr + acc, 0);
+}
+exports.getPolygonPerimeter = getPolygonPerimeter;
+function triangleCheck(sideA, sideB, sideC) {
+    const checkSideA = sideB - sideC < sideA && sideA < sideB + sideC;
+    const checkSideB = sideA - sideC < sideB && sideB < sideA + sideC;
+    const checkSideC = sideA - sideB < sideC && sideC < sideA + sideB;
+    return checkSideA && checkSideB && checkSideC;
+}
+exports.triangleCheck = triangleCheck;
--- backend/typescript-pratica/exercises.ts
+++ backend/typescript-pratica/exercises.ts
@@ -3,3 +3,22 @@
 export function getSquareArea(side: number): number {
   return side ** 2;
 }
+export function getRectangleArea(side: number, base: number): number {
+  return side * base;
+}
+export function getTriangleArea(height: number, base: number): number {
+  return (height * base) / 2;
+}
+export function getPolygonPerimeter(sides: number[]): number {
+  return sides.reduce((curr, acc) => curr + acc, 0);
+}
+export function triangleCheck(
+  sideA: number,
+  sideB: number,
+  sideC: number
+): boolean {
+  const checkSideA = sideB - sideC < sideA && sideA < sideB + sideC;
+  const checkSideB = sideA - sideC < sideB && sideB < sideA + sideC;
+  const checkSideC = sideA - sideB < sideC && sideC < sideA + sideB;
+  return checkSideA && checkSideB && checkSideC;
+}

aa308b44a6da52a698dd4f7da6e37b9a312b41ad
Breno Araujo <brenoaraujo50@icloud.com>
2022-12-14 12:15:30 +0000

    cria interface de pizza

---
+++ backend/typescript-pratica/pizza.ts
@@ -0,0 +1,17 @@
+interface pizza {
+  flavor: string;
+  slices: 4 | 6 | 8;
+}
+
+const pizza1: pizza = {
+  flavor: "Calabresa",
+  slices: 8
+}
+const pizza2: pizza = {
+  flavor: "Marguerita",
+  slices: 6
+}
+const pizza3: pizza = {
+  flavor: "Nutela",
+  slices: 4
+}

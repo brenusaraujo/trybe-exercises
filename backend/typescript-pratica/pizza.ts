2502d9211a14bcd0085b717c3d828d593d53c6cf
Breno Araujo <brenoaraujo50@icloud.com>
2022-12-14 12:27:44 +0000

    adiciona extensão da interfaceã

--- backend/typescript-pratica/pizza.ts
+++ backend/typescript-pratica/pizza.ts
@@ -1,17 +1,35 @@
+type numberOfSlices = 4 | 6 | 8;
+
 interface pizza {
   flavor: string;
-  slices: 4 | 6 | 8;
+  slices: numberOfSlices;
 }
 
 const pizza1: pizza = {
   flavor: "Calabresa",
   slices: 8
-}
+};
 const pizza2: pizza = {
   flavor: "Marguerita",
   slices: 6
-}
+};
 const pizza3: pizza = {
   flavor: "Nutela",
   slices: 4
+};
+type common = "Calabresa" | "Frango" | "Pepperoni";
+
+interface PizzaCommon extends pizza {
+  flavor: common;
+}
+
+type vegetarian = "Marguerita" | "Palmito" | "Cogumelo";
+type sweet = "Nutela" | "Goiabada com Queijo" | "Marshmallow";
+
+interface PizzaVegetarian extends pizza {
+  flavor: vegetarian;
+}
+interface PizzaSweet extends pizza {
+  flavor: sweet;
+  slices: 4;
 }
